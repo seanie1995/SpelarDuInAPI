e19ea2bbeeebba4e1f11fcb940fd751995fa159d
jonnytouma <jonnytouma@gmail.com>
2024-01-10 13:05:43 +0100

    Added Tesxt file wrote conditions for the assignment, and also assigned the first weeks workload in program.cs

--- SpelarDuInAPI/Program.cs
+++ SpelarDuInAPI/Program.cs
@@ -21,28 +21,23 @@ namespace SpelarDuInAPI
             // Endpoints to be added here 
 
             // GET Calls
-
-            app.MapGet("/user"); // Hämta alla personer
-
-            app.MapGet("/user/{userId}/genre"); // Hämta alla genre kopplad till en specifik person
-
-            app.MapGet("/user/{userId}/artist"); // Hämta alla artister kopplad till en specifik person
-
-            app.MapGet("/user/{userId}/track"); // Hämta alla tracks kopplad till en specifik person
+            /*
+            app.MapGet("/user"); // Hämta alla personer     Mojtaba
+            app.MapGet("/user/{userId}/genre"); // Hämta alla genre kopplad till en specifik person     Sean
+            app.MapGet("/user/{userId}/artist"); // Hämta alla artister kopplad till en specifik person     Jing
+            app.MapGet("/user/{userId}/track"); // Hämta alla tracks kopplad till en specifik person        Jonny
 
             // POST Calls
 
-            app.MapPost("/user"); //skapa ny user
-            app.MapPost("/genre"); //skapa ny genre
-            app.MapPost("/artist"); //skapa ny artist
-            app.MapPost("/track"); //skapa ny track
+            app.MapPost("/user"); //skapa ny user   Mojtaba
+            app.MapPost("/genre"); //skapa ny genre     Sean
+            app.MapPost("/artist"); //skapa ny artist   Jing
+            app.MapPost("/track"); //skapa ny track     jonny
 
-            app.MapPost("/user/{userId}/genre/{genreId}"); // Kopplar person till ny genre
-
-            app.MapPost("/user/{userId}/artist/{artistId}"); //  Kopplar person till ny artist
-
-            app.MapPost("/user/{userId}/track/{trackId}"); // Kopplar person till ny track
-
+            app.MapPost("/user/{userId}/genre/{genreId}"); // Kopplar person till ny genre  N/A
+            app.MapPost("/user/{userId}/artist/{artistId}"); //  Kopplar person till ny artist  N/A
+            app.MapPost("/user/{userId}/track/{trackId}"); // Kopplar person till ny track  N/A
+            */
             app.Run();
         }
     }
