d432544758a635ba7e31c4968f8074c76c0fdf91
jonnytouma <jonnytouma@gmail.com>
2024-01-15 16:12:25 +0100

    formating line 5 in TrackDto

--- SpelarDuInAPI/Models/DTO/TrackDto.cs
+++ SpelarDuInAPI/Models/DTO/TrackDto.cs
@@ -2,7 +2,7 @@
 {
     public class TrackDto
     {
-       public int Id { get; set; }
+        public int Id { get; set; }
         public string TrackTitle { get; set; }
         public string Genre { get; set; }
         public string Artist { get; set; }
