36aedf713ae573d2d404a604c40bde453afb6509
jonnytouma <jonnytouma@gmail.com>
2024-01-16 16:46:23 +0100

    started with unit test for AddNewTrack

--- SpelarDuInTest/UnitTest1.cs
+++ SpelarDuInTest/UnitTest1.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using SpelarDuInAPI.Data;
 using SpelarDuInAPI.Handlers;
+using SpelarDuInAPI.Models;
 using SpelarDuInAPI.Models.DTO;
 
 namespace SpelarDuInTest
@@ -50,6 +51,31 @@ namespace SpelarDuInTest
             // Assert
             
             Assert.AreEqual("Vegetarian Slamcore", addedGenre.GenreName);
+        }
+        [TestMethod]
+        public void AddNewTrack_AddTrackViaHandler()
+        {
+            // Arrange
+            DbContextOptions<ApplicationContext> options = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseInMemoryDatabase("TestDb")
+                .Options;
+            ApplicationContext context = new ApplicationContext(options);
+
+            TrackDto track = new TrackDto()
+            {
+                TrackTitle = "Title",
+                Artist = "ArtistTitle",
+                Genre = "Genre"
+            };
+            // Act
+            TrackHandler.AddNewTrack(context, track);
+
+            //Assert
+            Assert.AreEqual(1, context.Tracks.Count());
+            TrackDto actual = context.Tracks.Single();
+            Assert.AreEqual(track.TrackTitle, actual.TrackTitle);
+            Assert.AreEqual(track.Artist, actual.Artist);
+            Assert.AreEqual(track.Genre, actual.Genre);
         }
     }
 }
