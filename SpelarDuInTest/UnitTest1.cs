03180a25d2f9fd1dfa31515065ece8c5d36437b8
Sean Schelin <119659530+seanie1995@users.noreply.github.com>
2024-01-17 08:51:23 +0100

    Commented out UnitTest1. Not needed

--- SpelarDuInTest/UnitTest1.cs
+++ SpelarDuInTest/UnitTest1.cs
@@ -1,55 +1,55 @@
-using Microsoft.EntityFrameworkCore;
-using SpelarDuInAPI.Data;
-using SpelarDuInAPI.Handlers;
-using SpelarDuInAPI.Models.DTO;
+//using Microsoft.EntityFrameworkCore;
+//using SpelarDuInAPI.Data;
+//using SpelarDuInAPI.Handlers;
+//using SpelarDuInAPI.Models.DTO;
 
-namespace SpelarDuInTest
-{
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]       
-        public void AddNewGenre_AddGenreViaHandler()
-        {
-            // Arrange
-            DbContextOptions<ApplicationContext> options = new DbContextOptionsBuilder<ApplicationContext>()
-                .UseInMemoryDatabase("TestDb")
-                .Options;
-            ApplicationContext context = new ApplicationContext(options);              
+//namespace SpelarDuInTest
+//{
+//    [TestClass]
+//    public class UnitTest1
+//    {
+//        [TestMethod]       
+//        public void AddNewGenre_AddGenreViaHandler()
+//        {
+//            // Arrange
+//            DbContextOptions<ApplicationContext> options = new DbContextOptionsBuilder<ApplicationContext>()
+//                .UseInMemoryDatabase("TestDb")
+//                .Options;
+//            ApplicationContext context = new ApplicationContext(options);              
 
-            // Act
-            GenreHandler.AddNewGenre(context, new GenreDto()
-            {
-                GenreName = "Vegetarian Slamcore"
-            });
+//            // Act
+//            GenreHandler.AddNewGenre(context, new GenreDto()
+//            {
+//                GenreName = "Vegetarian Slamcore"
+//            });
 
-            // Assert
-            Assert.AreEqual(1, context.Genres.Count());
+//            // Assert
+//            Assert.AreEqual(1, context.Genres.Count());
 
-            var addedGenre = context.Genres.Where(x => x.GenreName == "Vegetarian Slamcore").First(); ;
+//            var addedGenre = context.Genres.Where(x => x.GenreName == "Vegetarian Slamcore").First(); ;
 
-            Assert.AreEqual("Vegetarian Slamcore", addedGenre.GenreName);
-        }
+//            Assert.AreEqual("Vegetarian Slamcore", addedGenre.GenreName);
+//        }
 
-        [TestMethod]
-        public void AddNewGenre_AddGenreViaHandlerCheckName()
-        {
-            // Arrange
-            DbContextOptions<ApplicationContext> options = new DbContextOptionsBuilder<ApplicationContext>()
-                .UseInMemoryDatabase("TestDb")
-                .Options;
-            ApplicationContext context = new ApplicationContext(options);
+//        [TestMethod]
+//        public void AddNewGenre_AddGenreViaHandlerCheckName()
+//        {
+//            // Arrange
+//            DbContextOptions<ApplicationContext> options = new DbContextOptionsBuilder<ApplicationContext>()
+//                .UseInMemoryDatabase("TestDb")
+//                .Options;
+//            ApplicationContext context = new ApplicationContext(options);
 
-            // Act
-            GenreHandler.AddNewGenre(context, new GenreDto()
-            {
-                GenreName = "Vegetarian Slamcore"
-            });
+//            // Act
+//            GenreHandler.AddNewGenre(context, new GenreDto()
+//            {
+//                GenreName = "Vegetarian Slamcore"
+//            });
 
-            var addedGenre = context.Genres.Where(x => x.GenreName == "Vegetarian Slamcore").First(); ;
-            // Assert
+//            var addedGenre = context.Genres.Where(x => x.GenreName == "Vegetarian Slamcore").First(); ;
+//            // Assert
             
-            Assert.AreEqual("Vegetarian Slamcore", addedGenre.GenreName);
-        }
-    }
-}
+//            Assert.AreEqual("Vegetarian Slamcore", addedGenre.GenreName);
+//        }
+//    }
+//}
