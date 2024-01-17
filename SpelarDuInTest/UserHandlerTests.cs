8296bba0d4668c2269ba17990e601167a7318b26
mmxzps <mojtaba.mobasheri@hotmail.com>
2024-01-17 19:03:34 +0100

    Added DbHelper instead of context for "CreateUser" act

---
+++ SpelarDuInTest/UserHandlerTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpelarDuInAPI.Data;
+using SpelarDuInAPI.Handlers;
+using SpelarDuInAPI.Models;
+using SpelarDuInAPI.Models.DTO;
+using SpelarDuInAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpelarDuInTest
+{
+    [TestClass]
+    public class UserHandlerTests
+    {
+        [TestMethod]
+        public void CreateUser_Works()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<ApplicationContext>().UseInMemoryDatabase(databaseName: "test-db").Options;
+            var context = new ApplicationContext(options);
+            UserDbHelper dbHelper = new UserDbHelper(context);
+
+            //Act
+            UserHandler.CreateUser(dbHelper, new UserDto()
+            {
+                UserName = "Test-user",
+            });
+            context.SaveChanges();
+            //Assert
+            Assert.AreEqual(1, context.Users.Count());
+        }
+
+        [TestMethod]
+        public void CreateUser_CorrectName()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<ApplicationContext>().UseInMemoryDatabase(databaseName: "test-db").Options;
+            var context = new ApplicationContext(options);
+            UserDbHelper dbHelper = new UserDbHelper(context);
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
