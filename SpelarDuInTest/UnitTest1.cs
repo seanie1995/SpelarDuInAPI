40f42c4450170420cf379dbe89369f1f523586ff
jonnytouma <jonnytouma@gmail.com>
2024-01-17 11:33:43 +0100

    Finished test for AddNewTrack
    it works with dbHelper but it will change for mock later

--- SpelarDuInTest/UnitTest1.cs
+++ SpelarDuInTest/UnitTest1.cs
@@ -1,22 +1,28 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using SpelarDuInAPI.Data;
 using SpelarDuInAPI.Handlers;
 using SpelarDuInAPI.Models;
 using SpelarDuInAPI.Models.DTO;
+using System.ComponentModel;
+using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using Newtonsoft.Json;
 
 namespace SpelarDuInTest
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]       
+        [TestMethod]
         public void AddNewGenre_AddGenreViaHandler()
         {
             // Arrange
             DbContextOptions<ApplicationContext> options = new DbContextOptionsBuilder<ApplicationContext>()
                 .UseInMemoryDatabase("TestDb")
                 .Options;
-            ApplicationContext context = new ApplicationContext(options);              
+            ApplicationContext context = new ApplicationContext(options);
 
             // Act
             GenreHandler.AddNewGenre(context, new GenreDto()
@@ -49,7 +55,7 @@ namespace SpelarDuInTest
 
             var addedGenre = context.Genres.Where(x => x.GenreName == "Vegetarian Slamcore").First(); ;
             // Assert
-            
+
             Assert.AreEqual("Vegetarian Slamcore", addedGenre.GenreName);
         }
         [TestMethod]
@@ -72,10 +78,27 @@ namespace SpelarDuInTest
 
             //Assert
             Assert.AreEqual(1, context.Tracks.Count());
-            TrackDto actual = context.Tracks.Single();
+            Track actual = context.Tracks.Single();
             Assert.AreEqual(track.TrackTitle, actual.TrackTitle);
-            Assert.AreEqual(track.Artist, actual.Artist);
-            Assert.AreEqual(track.Genre, actual.Genre);
+            Assert.AreEqual(track.Artist, actual.Artist.ArtistName);
+            Assert.AreEqual(track.Genre, actual.Genre.GenreName);
         }
+      /*  [TestMethod]
+        public void GetAllTracksFromSingleUser_WithNonExistingUserId_ReturnsNotFound()
+        {
+            // Arrange
+            DbContextOptions<ApplicationContext> options = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseInMemoryDatabase("TestDb")
+                .Options;
+            ApplicationContext context = new ApplicationContext(options);
+            var httpContext = new DefaultHttpContext();
+
+            // Act
+            TrackHandler.GetAllTracksFromSingleUser(context, 3);
+
+            //Assert
+
+            Assert.AreEqual(StatusCodes.Status404NotFound, httpContext.Response.StatusCode);
+        }*/
     }
 }
