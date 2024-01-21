51f5dfc68da2b1921b2e88119b3001b0706cf145
lucine1029 <sherry.jing@hotmail.com>
2024-01-21 20:55:29 +0100

    Add IArtistDbHelper, ArtistDbHelperTests and ArtistHandlerTests

--- SpelarDuInAPI/Handlers/ArtistHandler.cs
+++ SpelarDuInAPI/Handlers/ArtistHandler.cs
@@ -4,6 +4,7 @@ using SpelarDuInAPI.Data;
 using SpelarDuInAPI.Models;
 using SpelarDuInAPI.Models.DTO;
 using SpelarDuInAPI.Models.ViewModels;
+using SpelarDuInAPI.Services;
 using System.Net;
 
 namespace SpelarDuInAPI.Handlers
@@ -11,61 +12,17 @@ namespace SpelarDuInAPI.Handlers
     public class ArtistHandler
     {
         //Get all artists linked to a specific person --- Jing
-        public static IResult ListUsersArtists(ApplicationContext context, int userId)
+        public static IResult ListUsersArtists(IArtistDbHelper artistDbHelper, int userId)
         {
-            //find the speicific user
-            User? user = 
-                context.Users
-                .Where(a => a.Id == userId)
-                .Include(a => a.Artists)
-                .SingleOrDefault();
-
-            if (user == null)
-            {
-                return Results.NotFound("User has not found.");
-            }
-
-        // show all artists
-
-            ArtistViewModel[] result = user.Artists
-                .Select(r => new ArtistViewModel
-                {
-                    Id = r.Id,                      //need to show this Id? 
-                    ArtistName = r.ArtistName,
-                    Description = r.Description
-                }).ToArray();
+            ArtistViewModel[] result = artistDbHelper.ListUsersArtists(userId);
 
             return Results.Json(result);
-                
         }
 
         //Add an new artist --- Jing
-        public static IResult AddNewArtist(ApplicationContext context, ArtistDto artistDto)
+        public static void AddNewArtist(IArtistDbHelper artistDbHelper, ArtistDto artistDto)
         {
-            //vertify if the new artist's name has exists or empty first
-            var allArtists =
-                context.Artists
-                .ToArray();
-
-            if (allArtists.Any(a => a.ArtistName == artistDto.ArtistName))
-            {
-                return Results.Conflict($"{artistDto.ArtistName} has exists.");
-            }
-            else if (string.IsNullOrEmpty(artistDto.ArtistName))
-            {
-                return Results.Conflict("New artist's name must not be empty.");
-            }
-            //then add an new artist
-            else
-            {
-                context.Artists.Add(new Artist()
-                {
-                    ArtistName = artistDto.ArtistName,
-                    Description = artistDto.Description
-                });
-                context.SaveChanges();
-                return Results.StatusCode((int)HttpStatusCode.Created);
-            }
+            artistDbHelper.AddNewArtist(artistDto);
         }
     }
 }
