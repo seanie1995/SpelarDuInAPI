70554d8bb5baaf22f8e3aa9ba7590c87a989b784
lucine1029 <sherry.jing@hotmail.com>
2024-01-23 19:15:27 +0100

    Updated ArtistHandlerTests and ArtistDbHelperTests

--- SpelarDuInTest/ArtistDbHelperTests.cs
+++ SpelarDuInTest/ArtistDbHelperTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SpelarDuInAPI.Data;
+using SpelarDuInAPI.Models.DTO;
 using SpelarDuInAPI.Services;
 using System;
 using System.Collections.Generic;
@@ -12,8 +13,9 @@ namespace SpelarDuInTest
     [TestClass]
     public class ArtistDbHelperTests
     {
+        //Here we are testing to see if the changes in db goes through correctly!
         [TestMethod]
-        public void AddArtist_AddNewArtistToDb()
+        public void AddArtist_successfullyAddNewArtistToDb()
         {
             //Arrange
             DbContextOptions<ApplicationContext> options = new DbContextOptionsBuilder<ApplicationContext>()
@@ -32,5 +34,51 @@ namespace SpelarDuInTest
             //Assert
             Assert.AreEqual(1, context.Artists.Count());
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void AddNewArtist_AddExistingArtistGivesException()
+        {
+            // Arrange
+            DbContextOptions<ApplicationContext> options = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseInMemoryDatabase("TestDb")
+                .Options;
+            ApplicationContext context = new ApplicationContext(options);
+            ArtistDbHelper artistDbHelper = new ArtistDbHelper(context);
+
+            artistDbHelper.AddNewArtist(new ArtistDto()
+            {
+                ArtistName = "TestArtistName",
+                Description = "TestDescription"
+            });
+
+            // Act
+
+            artistDbHelper.AddNewArtist(new ArtistDto()
+            {
+                ArtistName = "TestArtistName",
+                Description = "TestDescription"
+            });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void AddNewArtist_AddEmptyArtistGivesException()
+        {
+            // Arrange
+            DbContextOptions<ApplicationContext> options = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseInMemoryDatabase("TestDb")
+                .Options;
+            ApplicationContext context = new ApplicationContext(options);
+            ArtistDbHelper artistDbHelper = new ArtistDbHelper(context);
+
+            // Act
+            artistDbHelper.AddNewArtist(new ArtistDto()
+            {
+                ArtistName = null,
+                Description = null
+            });
+        }
+
     }
 }
